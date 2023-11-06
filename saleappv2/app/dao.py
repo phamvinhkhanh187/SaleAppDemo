74db3706228f07fe25a6e84b72fbba9bb22cb268
dhthanhqa@gmail.com <dhthanhqa@gmail.com>
2023-11-06 11:31:48 +0700

    add mysql

--- saleappv2/app/dao.py
+++ saleappv2/app/dao.py
@@ -1,53 +1,14 @@
+from app.models import Category, Product
+
+
 def get_categories():
-    return [{
-        "id": 1,
-        "name": "Mobile"
-    }, {
-        "id": 2,
-        "name": "Tablet"
-    }]
+    return Category.query.all()
 
 
 def get_products(kw):
-    products = [{
-        "id": 1,
-        "name": "iPhone 13",
-        "price": 20000000,
-        "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg",
-        "category_id": 1
-    }, {
-        "id": 2,
-        "name": "Galaxy S23 Plus",
-        "price": 20000000,
-        "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg",
-        "category_id": 1
-    }, {
-        "id": 1,
-        "name": "iPhone 13",
-        "price": 20000000,
-        "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg",
-        "category_id": 1
-    }, {
-        "id": 1,
-        "name": "iPhone 13",
-        "price": 20000000,
-        "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg",
-        "category_id": 1
-    }, {
-        "id": 1,
-        "name": "iPhone 13",
-        "price": 20000000,
-        "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg",
-        "category_id": 1
-    }, {
-        "id": 1,
-        "name": "iPhone 13",
-        "price": 20000000,
-        "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg",
-        "category_id": 1
-    }]
+    products = Product.query
 
     if kw:
-        products = [p for p in products if p['name'].find(kw) >= 0]
+        products = products.filter(Product.name.contains(kw))
 
-    return products
+    return products.all()
---
+++ saleappv2/app/models.py
@@ -0,0 +1,41 @@
+from sqlalchemy import Column, Integer, String, Float, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
+from app import db, app
+
+
+class Category(db.Model):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(50), nullable=False, unique=True)
+    products = relationship('Product', backref='category', lazy=True)
+
+
+class Product(db.Model):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(50), nullable=False, unique=True)
+    price = Column(Float, default=0)
+    image = Column(String(100),
+                   default='https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg')
+    active = Column(Boolean, default=True)
+    category_id = Column(Integer, ForeignKey(Category.id), nullable=False)
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        # db.create_all()
+        # c1 = Category(name='Mobile')
+        # c2 = Category(name='Tablet')
+        # c3 = Category(name='Desktop')
+        # db.session.add(c1)
+        # db.session.add(c2)
+        # db.session.add(c3)
+        # db.session.commit()
+        p1 = Product(name='iPhone 13', price=22000000, category_id=1)
+        p2 = Product(name='Galaxy Tab S9', price=28000000, category_id=2)
+        p3 = Product(name='iPad Pro 2023', price=21000000, category_id=2)
+        p4 = Product(name='Galaxy S23', price=18000000, category_id=1)
+        p5 = Product(name='iPhone 15', price=22000000, category_id=1)
+        db.session.add_all([p1, p2, p3, p4, p5])
+        db.session.commit()
+
