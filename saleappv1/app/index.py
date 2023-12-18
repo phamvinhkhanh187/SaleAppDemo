b23bec0ae961f1e079805d065d4ae0aa945a743c
dhthanhqa@gmail.com <dhthanhqa@gmail.com>
2023-12-18 11:35:41 +0700

    update cart

--- saleappv1/app/index.py
+++ saleappv1/app/index.py
@@ -1,7 +1,8 @@
 import math
 
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, jsonify, session
 import dao
+import utils
 from app import app, login
 from flask_login import login_user
 
@@ -12,13 +13,12 @@ def index():
     cate_id = request.args.get('cate_id')
     page = request.args.get('page')
 
-    cates = dao.get_categories()
     prods = dao.get_products(kw, cate_id, page)
 
     num = dao.count_product()
     page_size = app.config['PAGE_SIZE']
 
-    return render_template('index.html', categories=cates,
+    return render_template('index.html',
                            products=prods, pages=math.ceil(num/page_size))
 
 
@@ -32,6 +32,59 @@ def login_admin():
         login_user(user)
 
     return redirect('/admin')
+
+
+@app.route("/cart")
+def cart():
+    return render_template('cart.html')
+
+
+@app.route("/api/cart", methods=['post'])
+def add_to_cart():
+    data = request.json
+
+    cart = session.get('cart')
+    if cart is None:
+        cart = {}
+
+    id = str(data.get("id"))
+    if id in cart:
+        cart[id]['quantity'] += 1
+    else:
+        cart[id] = {
+            "id": id,
+            "name": data.get("name"),
+            "price": data.get("price"),
+            "quantity": 1
+        }
+
+    session['cart'] = cart
+
+    """
+        {
+            "1": {
+                "id": "1",
+                "name": "...",
+                "price": 123,
+                "quantity": 2
+            },  "2": {
+                "id": "2",
+                "name": "...",
+                "price": 1234,
+                "quantity": 1
+            }
+        }
+    """
+
+    return jsonify(utils.count_cart(cart))
+
+
+@app.context_processor
+def common_responses():
+    return {
+        'categories': dao.get_categories(),
+        'cart_stats': utils.count_cart(session.get('cart'))
+    }
 
 
 @login.user_loader
--- saleappv2/app/__init__.py
+++ saleappv2/app/__init__.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.secret_key = '&%^&)7896987697*%^%&*^)*^*RTUYTIUY*^&%&*^%&(^(*'
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:%s@localhost/saledbv2?charset=utf8mb4" % quote('Admin@123')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
-app.config["PAGE_SIZE"] = 2
+app.config["PAGE_SIZE"] = 6
 
 db = SQLAlchemy(app=app)
 login = LoginManager(app=app)
